bbc564b3cd230896834fc7ac904a7ad35e994ce3
beckgoodloe <beckgoodloe@gmail.com>
2014-06-11 10:10:25 -0500

    dynamic urls for bible pages

--- routes/bible.js
+++ routes/bible.js
@@ -9,6 +9,12 @@ exports.bible = function(){
 
 exports.section = function(){
     return function(req,res){
-        
+        var section = req.url
+        section = section.substring(section.lastIndexOf('/')+1)
+        res.render('biblesection', {
+            cooke:req.session.loggedin,
+            session:req.session,
+            section:section
+        })
     }
 };
