00e902a3cb012b4b4e44c20a8b2e418f198665ad
Ryan Rice <ryanr1230@gmail.com>
2014-08-18 00:29:25 -0500

    fixing for deploy

--- db.js
+++ db.js
@@ -2,5 +2,5 @@
 
 var monk = require('monk');
 var mongo = require('mongodb');
-module.exports = monk('localhost:27017/lasauil'); //using test database
-// module.exports = monk('lasauil:lasauil@novus.modulusmongo.net:27017/mogAh5az'); //using real database
+// module.exports = monk('localhost:27017/lasauil'); //using test database
+module.exports = monk('lasauil:lasauil@novus.modulusmongo.net:27017/mogAh5az'); //using real database
