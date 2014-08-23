d7dbffa882c733d3cce78e79723bcbc6aaaf0999
Ryan Rice <ryanr1230@gmail.com>
2014-08-22 20:15:20 -0500

    no real differences, testing newer version of nodemailer to send emails

--- routes/feedback/feedback.js
+++ routes/feedback/feedback.js
@@ -12,27 +12,29 @@ module.exports = function(app) {
     });
 
     app.all('/sendfeedback', function (req, res) {
-        var name = req.body.name;
-        var subject = req.body.subject;
-        var text = req.body.text;
-        var email = req.body.email;
-        var smtpTransport = nodemailer.createTransport("SMTP", {
-            service: "Gmail",
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
             auth: {
                 user: config.feedback.email,
                 pass: config.feedback.password
             }
         });
+        var name = req.body.name;
+        var subject = req.body.subject;
+        var text = req.body.text;
+        var email = req.body.email;
         var mailOptions = {
-            to: config.feedback.email,
-            from: name,
-            subject: subject,
+            from: name, 
+            to: config.feedback.email, 
+            subject: subject, 
             text: text + "\n\nRespond to this person at: " + email
-        }
-        smtpTransport.sendMail(mailOptions, function (err, response) {
+        };
+
+        transporter.sendMail(mailOptions, function (err, response) {
             if (err) {
                 throw err;
             } else {
+                console.log("Message sent   " + response.response);
                 res.redirect("/");
             }
         });
