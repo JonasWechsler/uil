f23cff8d960d732f26423fc2adcd2b056a2e514f
Ryan Rice <ryanr1230@gmail.com>
2014-08-24 21:37:51 -0500

    adding view for viewing sorted reported questions

--- routes/report/report.js
+++ routes/report/report.js
@@ -1,6 +1,16 @@
 var monk = require('monk');
 var mongo = require('mongodb');
 var db = require('../../common/db');
+//reverse sorting
+var questionreportedcompare = function(a, b) {
+	var areported = a.reported?a.reported:0;
+	var breported = b.reported?b.reported:0;
+	if(areported > breported) 
+		return -1;
+	if(areported < breported)
+		return 1;
+	return 0;
+}
 
 module.exports = function(app) {
     app.get('/report/:id', function (req, res) {
@@ -16,6 +26,20 @@ module.exports = function(app) {
 	    			}
 	    		);
 	    	}
+    	});
+    });
+
+    app.get('/reported', function (req,res) {
+    	var questions = db.get("questions");
+    	questions.find({}, function(err, questions) {
+    		questions.sort(questionreportedcompare);
+    		if(!err && questions) {
+                res.render('report/reported', {
+                    questionlist: questions,
+                    prompt: 'Reported questions sorted by times reported',
+                    session: req.session
+                });
+    		}
     	});
     });
 }
