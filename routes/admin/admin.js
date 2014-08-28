29f243e6fd6ad8a7c1eaa3fd3b7b12851d01077b
patil215 <neilpatil215@gmail.com>
2014-08-28 13:04:02 -0500

    Add and remove admin

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,15 +1,28 @@
 var db = require('../../common/db');
+var async = require('async');
 var common = {}
 common.utils = require('../../common/utils');
 module.exports = function(app) {
 	app.all('/admin', function (req, res) {
         common.utils.verifyAdmin(req.session.id, function(isAdmin) {
             if(isAdmin) {
-                db.get('users').find({}, function(err, users) { 
-                    res.render('admin/admin', {
-                        admin: isAdmin,
-                        users: users,
-                        session: req.session
+                db.get('users').find({}, function(err, users) {
+                    db.get('admins').find({}, function(err, admins) {
+                        var adminNames = [];
+                        async.each(admins, function(admin, completed) {
+                            db.get('users').find({_id : admin.user}, function(err, users) {
+                                adminNames.push(users[0].username);
+                                completed();
+                            });
+                        }, function(err) {
+                            console.log(adminNames);
+                            res.render('admin/admin', {
+                                admin: isAdmin,
+                                users: users,
+                                admins: adminNames,
+                                session: req.session
+                            });
+                        });
                     });
                 });
             } else {
@@ -18,16 +31,47 @@ module.exports = function(app) {
         });
 	});
 
+    app.all('/removeadmin', function(req, res) {
+        db.get('users').find({"username": req.body.username}, function(err, users) {
+            if(users.length > 0) {
+                db.get('admins').remove({
+                    "user": users[0]._id
+                }, function(err, bear) {
+                    db.get('users').find({}, function(err, users) { 
+                        res.redirect('/admin');
+                    });
+                });
+            }
+        });
+        
+    });
+    app.all('/addadmin', function(req, res) {
+        db.get('users').find({"username": req.body.username}, function(err, users) {
+            if(err) {
+                res.redirect('/admin');
+            } else {
+                if(users.length > 0) {
+                    db.get('admins').insert({"user":users[0]._id}, function(err) {
+                        res.redirect('/admin');
+                    });
+                }
+            }
+        });
+    });
+
     app.all('/removeuser', function(req, res) {
         db.get('users').remove({
             "username": req.body.username
         }, function(err, bear) {
-            db.get('users').find({}, function(err, users) { 
-                    res.redirect('/admin');
-            });
+            if(err) {
+                res.redirect('/admin');
+            } else {
+                db.get('users').find({}, function(err, users) { 
+                        res.redirect('/admin');
+                });
+            }
         });
     });
-
     app.all('/changeuserscore', function(req, res) {
         db.get('users').find({"username": req.body.username}, function(err, users) {
             if(users.length > 0) {
