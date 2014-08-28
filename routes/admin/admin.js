b1c60b6222256c2ba3173cb81119a91d6316e563
patil215 <neilpatil215@gmail.com>
2014-08-28 09:44:26 -0500

    Somewhat useful things in admin page

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,36 +1,29 @@
 var db = require('../../common/db');
+var common = {}
+common.utils = require('../../common/utils');
 module.exports = function(app) {
 	app.all('/admin', function (req, res) {
-		if (req.session.loggedin) {
-            var username = req.session.user;
-            var users = db.get('users');
-            users.findOne({
-            	'username':username
-            }, function(err, user) {
-            	var userId = user._id;
-            	console.log(userId);
-            	var admins = db.get('admins');
-            	admins.findOne({
-            		'user':String(userId)
-            	}, function (err, found) {
-                	if (err) {
-                    	throw err;
-                	} else if (found) {
-                    	res.render('admin/admin', {
-                    		admin: "YOU ARE ADMIN"
-                    	});
-                	} else {
-                		res.render('admin/admin', {
-                    		admin: "LOSER YOU ARE NOT ADMIN"
-                    	});
-                    }
+        common.utils.verifyAdmin(req.session.id, function(isAdmin) {
+            if(isAdmin) {
+                db.get('users').find({}, function(err, users) { 
+                    res.render('admin/admin', {
+                        admin: isAdmin,
+                        users: users
+                    });
                 });
-            });
-        } else {
-            res.render('index', {
-                title: 'LASA UIL Training',
-                session: req.session
-            });
-		}
+            } else {
+                res.redirect('/');
+            }
+        });
 	});
+
+    app.all('/removeuser', function(req, res) {
+        db.get('users').remove({
+            "username": req.body.username
+        }, function(err, bear) {
+            db.get('users').find({}, function(err, users) { 
+                    res.redirect('/admin');
+            });
+        });
+    });
 }
