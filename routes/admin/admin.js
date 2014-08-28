304fadfe1fbbe04edb184bb3b99f39ac7166a5d6
patil215 <neilpatil215@gmail.com>
2014-08-28 12:36:36 -0500

    Admin page can remove users, and change their scores and streaks

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -8,7 +8,8 @@ module.exports = function(app) {
                 db.get('users').find({}, function(err, users) { 
                     res.render('admin/admin', {
                         admin: isAdmin,
-                        users: users
+                        users: users,
+                        session: req.session
                     });
                 });
             } else {
@@ -24,6 +25,37 @@ module.exports = function(app) {
             db.get('users').find({}, function(err, users) { 
                     res.redirect('/admin');
             });
+        });
+    });
+
+    app.all('/changeuserscore', function(req, res) {
+        db.get('users').find({"username": req.body.username}, function(err, users) {
+            if(users.length > 0) {
+                users[0].score = Number(req.body.score);
+                db.get('users').update({"username":req.body.username}, users[0], function(err, count, status) {
+                    res.redirect('/admin');
+                });
+            }
+        });
+    });
+    app.all('/changeuserstreak', function(req, res) {
+        db.get('users').find({"username": req.body.username}, function(err, users) {
+            if(users.length > 0) {
+                users[0].streak = Number(req.body.score);
+                db.get('users').update({"username":req.body.username}, users[0], function(err, count, status) {
+                    res.redirect('/admin');
+                });
+            }
+        });
+    });
+    app.all('/changeuserlongeststreak', function(req, res) {
+        db.get('users').find({"username": req.body.username}, function(err, users) {
+            if(users.length > 0) {
+                users[0].longeststreak = Number(req.body.score);
+                db.get('users').update({"username":req.body.username}, users[0], function(err, count, status) {
+                    res.redirect('/admin');
+                });
+            }
         });
     });
 }
