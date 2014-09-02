13eecd718af12f42c96ed97e30167921bc9236de
Ryan Rice <ryanr1230@gmail.com>
2014-09-02 10:43:48 -0400

    safe programs (java only)

--- routes/programming/programming.js
+++ routes/programming/programming.js
@@ -13,71 +13,67 @@ module.exports = function(app) {
     });
 
     app.post('/programming', function (req, res) {
-        common.utils.verifyAdmin(req.session.id, function(isAdmin) {
-            if(isAdmin) {
-                var saveName = req.files.upload.name;
-                //to ensure the correct file type ending
-                var cutoff = saveName.lastIndexOf('.' + req.body.type);
-                saveName = saveName.substring(0, cutoff === -1? saveName.lastIndexOf("."):cutoff);
-                //check against command line injection
-                if(!(/^[a-zA-Z0-9\.]*$/.test(saveName))) {
-                    res.render('programming/programming', {
-                        prompt: 'Invalid File Name',
-                        session:req.session
-                    });
-                    return;
-                }
-                common.utils.save(req.files.upload, "./programs", saveName + "." + req.body.type, function (err) {
-                    if (err) {
-                        console.log(err);
-                        res.render('programming/programming', {
-                            prompt: 'Could not save file',
-                            session: req.session
-                        });
-                    } else {
-
-                        input = "./programs/" + saveName + "." + req.body.type;
-                        var inputDir = path.dirname(input);
-                        var inputFile = path.basename(input);
-                        common.execute.compile(req.body.type, inputDir, inputFile, function(err, stdout, stderr) {
-                            if(err) {
-                                res.render('programming/programming', {
-                                    prompt: 'Could not compile' + err,
-                                    session: req.session
-                                });
-                            } else {
-                              common.execute.run(req.body.type, inputDir, inputFile, function(err, stdout, stderr){
-                                if (err) {
-                                    res.render('programming/programming', {
-                                        prompt: 'Could not execute' + err,
-                                        session: req.session
-                                    });
-                                } else {
-                                    fs.readFile(inputDir + '/' + inputFile, 'utf8', function(err,data) {
-                                        if(data) {
-                                            res.render('programming/programming', {
-                                                prompt: stdout,
-                                                programout: data,
-                                                session: req.session
-                                            });
-                                        } else {
-                                            res.render('programming/programming', {
-                                                prompt: stdout,
-                                                session: req.session
-                                            });
-                                        }
-                                    });
-
-                                }
-                              });
-                            }
-                        });
-                    }
+        var type = 'java';
+        var saveName = req.files.upload.name;
+        //to ensure the correct file type ending
+        var cutoff = saveName.lastIndexOf('.' + type);
+        saveName = saveName.substring(0, cutoff === -1? saveName.lastIndexOf("."):cutoff);
+        //check against command line injection
+        if(!(/^[a-zA-Z0-9\.]*$/.test(saveName))) {
+            res.render('programming/programming', {
+                prompt: 'Invalid File Name',
+                session:req.session
+            });
+            return;
+        }
+        common.utils.save(req.files.upload, "./programs", saveName + "." + type, function (err) {
+            if (err) {
+                console.log(err);
+                res.render('programming/programming', {
+                    prompt: 'Could not save file',
+                    session: req.session
                 });
             } else {
-                res.render('programming/programming', {
-                    prompt: 'submitting is only allowed for admins', 
-                    session: req.session
+
+                input = "./programs/" + saveName + "." + type;
+                var inputDir = path.dirname(input);
+                var inputFile = path.basename(input);
+                common.execute.compile(type, inputDir, inputFile, function(err, stdout, stderr) {
+                    if(err) {
+                        res.render('programming/programming', {
+                            prompt: 'Could not compile' + err,
+                            session: req.session
+                        });
+                        common.execute.remove([input]);
+                    } else {
+                      common.execute.run(type, inputDir, inputFile, function(err, stdout, stderr){
+                        if (err) {
+                            res.render('programming/programming', {
+                                prompt: 'Could not execute' + err,
+                                session: req.session
+                            });
+                            common.execute.remove([input]);
+                        } else {
+                            fs.readFile(inputDir + '/' + inputFile, 'utf8', function(err,data) {
+                                if(data) {
+                                    res.render('programming/programming', {
+                                        prompt: stdout,
+                                        programout: data,
+                                        session: req.session
+                                    });
+                                    common.execute.remove([inputDir + '/' + inputFile, inputDir + '/' + inputFile.substring(0,inputFile.lastIndexOf('.java')) + '.class']);
+                                } else {
+                                    res.render('programming/programming', {
+                                        prompt: stdout,
+                                        session: req.session
+                                    });
+                                    common.execute.remove([input]);
+                                }
+                            });
+
+                        }
+                      });
+                    }
                 });
             }
         });
